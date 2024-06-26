c7a77686d30367795ebff14be774af44678bcb93
edonis19 <131147865+edonis19@users.noreply.github.com>
2024-06-26 17:02:00 +0200

    Add files manually

---
+++ ApiProjectWork/ApiProjectWork/Entities/Transaction.cs
@@ -0,0 +1,17 @@
+﻿namespace ApiProjectWork.Entities
+{
+    public class Transaction
+    {
+        public int Id { get; set; }
+
+        public int ControllerId { get; set; }
+
+        public string UserId { get; set; } 
+
+        public decimal KwUsage { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public decimal TotalMoney { get; set; }
+    }
+}
