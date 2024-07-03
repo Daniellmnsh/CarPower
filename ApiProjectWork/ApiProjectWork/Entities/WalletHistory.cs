30ced44c67a876faa609092436df9bafa7a32b16
Andrea Fontanella <andrea.fontanella@stud.itsaltoadriatico.it>
2024-07-03 12:34:53 +0200

    aggiunta entrate e uscite

---
+++ ApiProjectWork/ApiProjectWork/Entities/WalletHistory.cs
@@ -0,0 +1,10 @@
+﻿namespace ApiProjectWork.Entities
+{
+    public class WalletHistory
+    {
+        public decimal Money { get; set; }
+        public DateTime Date { get; set; }
+        public int WalletId { get; set; }
+        public string UserId { get; set; }
+    }
+}
